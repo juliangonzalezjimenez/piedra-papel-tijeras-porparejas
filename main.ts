78d84f24bb641eca39dad98384b932d4ecd0879a
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-02-09 09:09:12 +0100

    Estoy guardando

--- main.ts
+++ main.ts
@@ -1,3 +1,60 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    Jugada2 = randint(1, 3)
+    if (Jugada2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Jugada2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
+input.onGesture(Gesture.Shake, function () {
+    MiJugada = randint(1, 3)
+    radio.sendNumber(MiJugada)
+    if (MiJugada == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (MiJugada == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+let MiJugada = 0
+let Jugada2 = 0
+radio.setGroup(255)
